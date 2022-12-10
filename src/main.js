c3abc2f5f6eb36f24b6a3ea7b9ab5d2a899c0313
dima-shishkin <119718232+dima-shishkin@users.noreply.github.com>
2022-12-10 14:46:20 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
-console.log(1);
+const generateComment = () => {
+    return {
+        id: 2,
+        messade: "Круто",
+        user: {
+            avatar: './img/avatars/1.svg',
+            name: "Дима",
+        }
+    };
+}
+
+const generatePicture = () => {
+    return {
+        id: 1,
+        url: './photos/1.jpg',
+        description: "фотографии природы",
+        likse: 200,
+        comments: [
+            generateComment(),
+            generateComment(),
+            generateComment(),
+        ],
+    };
+};
+
+const generatePictures = () => {
+    return [
+        generatePicture(),
+        generatePicture(),
+        generatePicture(),
+        generatePicture(),
+        generatePicture(),
+
+        generatePicture(),
+        generatePicture(),
+        generatePicture(),
+        generatePicture(),
+        generatePicture(),
+    ];
+}
+
+const pictures = generatePictures();
+console.log(pictures);
