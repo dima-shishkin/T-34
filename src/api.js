1b6102dd964a4c2c4a21769a8e59ce38c8010fa4
dima-shishkin <119718232+dima-shishkin@users.noreply.github.com>
2023-03-04 18:46:11 +1000

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -1,3 +1,10 @@
 function sendData() {
-    let xhr = new HMLHttpRequest();
+    const xhr = new HMLHttpRequest();
+    xhr.open('POST', 'http://localhost:80/pictures');
+    xhr.addEventListener('load', function () {
+        if (xhr.status === 201) {
+            console.log(1);
+        }
+    });
+    xhr.send();
 }
